dff874337fedf463d036bfc9be4489beba839ba3
Petrulli <alexandrepetrulli@hotmail.com>
2018-04-04 15:11:27 -0300

    Add files via upload

---
+++ costOfTile.py
@@ -0,0 +1,8 @@
+## Tile cost calculator
+
+tiles = float(input('How many tiles do you want to buy? '))
+price = float(input('what\'s the price of the tile? '))
+
+result = tiles * price
+print('%.2f'%result)
+
